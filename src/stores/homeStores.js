c2a904926e590b7affde17a18ca0389d71f87e85
Allan Heremi <allanheremidev@gmail.com>
2023-05-31 17:26:39 +0200

    added ability to search

--- src/stores/homeStores.js
+++ src/stores/homeStores.js
@@ -6,15 +6,23 @@ const homeStore = create(set => ({
   coins: [],
   query: '',
 
-  setQuery: (event) => {
-    set({query: event.target.value})
-    homeStore.getState().searchCoins()
+  setQuery: event => {
+    set({ query: event.target.value });
+    homeStore.getState().searchCoins();
   },
 
-  searchCoins: debounce( async() => {
-    const { query } = homeStore.getState()
-    const res = await axios.get(`https://api.coingecko.com/api/v3/search?query=${query}`)
-    console.log(res)
+  searchCoins: debounce(async () => {
+    const { query } = homeStore.getState();
+    const res = await axios.get(
+      `https://api.coingecko.com/api/v3/search?query=${query}`
+    );
+
+    const coins = res.data.coins.map(coin => {
+      return { 
+        name: coin.name, image: coin.large, id: coin.id 
+      }
+    });
+    set({coins})
   }, 750),
 
   fetchCoins: async () => {
