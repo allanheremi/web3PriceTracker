be880d3938189487e37587fdadb74cbd5e90b602
Allan Heremi <allanheremidev@gmail.com>
2023-06-01 08:51:48 +0200

    marketChartID from api implemented

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -7,11 +7,17 @@ const showStore = create(set => ({
   graphData: [],
 
   fetchData: async id => {
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=365`
-    );
+    const [graphRes, dataRes] = await Promise.all([
+      axios.get(
+        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=1225`
+      ),
+      axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`)
+    ])
 
-    const graphData = res.data.prices.map(price => {
+ 
+    // https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&market_data=true URL for price id
+
+    const graphData = graphRes.data.prices.map(price => {
       const [timestamp, p] = price;
       const date = new Date(timestamp).toLocaleDateString('en-us')
 
