6745ed2915a6eb93a17b16434eba91d669c34e86
Allan Heremi <allanheremidev@gmail.com>
2023-06-08 07:19:02 +0200

    header for showStore

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import showStore from '../stores/showStore';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
 const data = [
   {
@@ -48,36 +56,36 @@ const data = [
   },
 ];
 
-
-  
-
 export default function Show() {
-  const store = showStore()
-  const params = useParams()
+  const store = showStore();
+  const params = useParams();
 
   React.useEffect(() => {
-    store.fetchData(params.id)
-  }, [])
+    store.fetchData(params.id);
+  }, []);
 
   return (
-    <div>  <AreaChart
-    width={500}
-    height={400}
-    data={store.graphData}
-    margin={{
-      top: 10,
-      right: 30,
-      left: 0,
-      bottom: 0,
-    }}
-  >
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="Date" />
-    <YAxis />
-    <Tooltip />
-    <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
-  </AreaChart>
-
-</div>
-  )
+    <div>
+      <header>
+        <h2></h2>
+      </header>{' '}
+      <AreaChart
+        width={500}
+        height={400}
+        data={store.graphData}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="Date" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+    </div>
+  );
 }
