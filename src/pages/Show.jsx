e9b1391479a296f9aa199add29ccd7e432b0dddc
Allan Heremi <allanheremidev@gmail.com>
2023-06-09 08:18:19 +0200

    Market data finetuned (styling)

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -73,20 +73,16 @@ export default function Show() {
   return (
     <div>
       <Header back/>
-      <header>
+      <header className="show__Header">
         <img src={store.dataRes.image.large}/>
         <h2>{store.dataRes.name} ({(store.dataRes.symbol)})</h2>
       </header>
+      <div className="width">
+      <div className="show__Graph">
+        <ResponsiveContainer width="100%" height="100%">
       <AreaChart
-        width={500}
-        height={400}
         data={store.graphData}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
+       
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="Date" />
@@ -94,30 +90,45 @@ export default function Show() {
         <Tooltip />
         <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
       </AreaChart>
-      <div>
-        <h4>Market cap rank</h4>
+      </ResponsiveContainer>
+      </div>
+      </div>
+
+          <div className="width">
+        <div className="show__Details">
+        <h2>Market info</h2>
+
+      <div className='show__Details__Row'>
+        <h3>Market cap rank</h3>
         <span>#{store.dataRes.market_cap_rank}</span>
       </div>
-      <div>
-        <h4>24h high</h4>
+      <div className='show__Details__Row'>
+        <h3>24h high</h3>
         <span>${store.dataRes.market_data.high_24h.usd}</span>
       </div>
-      <h4>24h low</h4>
+      <div className='show__Details__Row'> 
+      <h3>24h low</h3>
       <span>${store.dataRes.market_data.low_24h.usd}</span>
-      <div>
-        <h4>Circulating supply</h4>
+      </div>
+      <div className='show__Details__Row'>
+        <h3>Circulating supply</h3>
         <span>{store.dataRes.market_data.circulating_supply.toFixed(0)}</span>
       </div>
-      <div>
-        <h4>Current price</h4>
+      <div className='show__Details__Row'>
+        <h3>Current price</h3>
         <span>${store.dataRes.market_data.current_price.usd}</span>
       </div>
-      <div>
-        <h4>1 year price change</h4>
+      <div className='show__Details__Row'>
+        <h3>1 year price change</h3>
         <span>{store.dataRes.market_data.price_change_percentage_1y.toFixed(3)}%</span>
       </div>
-      <h4>1 Month price change</h4>
+      <div className='show__Details__Row'>
+      <h3>1 Month price change</h3>
       <span>{store.dataRes.market_data.price_change_percentage_30d.toFixed(3)}%</span>
+      </div>
     </div>
+  </div>
+</div>
+
   );
 }
