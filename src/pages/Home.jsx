c9600dd369856e6d9db2599e0f1b2142b0774a35
Allan Heremi <allanheremidev@gmail.com>
2023-07-24 13:19:47 +0200

    Fix: removed homejsx effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import homeStore from '../stores/homeStores';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
@@ -6,47 +6,9 @@ import ListItem from '../components/ListItem';
 
 export default function Home() {
   const store = homeStore();
-  const searchRef = useRef(null);
 
   React.useEffect(() => {
     store.fetchCoins();
-  }, []);
-
-  useEffect(() => {
-    const letters = 'abcdefghijklkmnopqrstuvwxyz';
-    let interval = null;
-    const searchElement = searchRef.current;
-
-    const randomize = () => {
-      let iteration = 0;
-
-      clearInterval(interval);
-
-      interval = setInterval(() => {
-        searchElement.innerText = searchElement.innerText
-          .split('')
-          .map((letter, index) => {
-            if (index < iteration) {
-              return searchElement.dataset.value[index];
-            }
-
-            return letters[Math.floor(Math.random() * 26)];
-          })
-          .join('');
-
-        if (iteration >= searchElement.dataset.value.length) {
-          clearInterval(interval);
-        }
-
-        iteration += 1 / 1.75;
-      }, 30);
-    };
-
-    randomize();
-
-    return () => {
-      clearInterval(interval);
-    };
   }, []);
 
   return (
@@ -54,9 +16,7 @@ export default function Home() {
       <Header />
       <div className="width">
         <header className="home-search">
-          <h2 ref={searchRef} data-value="Search for a coin">
-            Search for a coin
-          </h2>
+          <h2>Search for a coin</h2>
           <input type="text" value={store.query} onChange={store.setQuery} />
         </header>
       </div>
@@ -64,7 +24,7 @@ export default function Home() {
         <div className="width">
           <h2>Trending coins</h2>
           <div className="home__Cryptos__List">
-            {store.coins.map((coin) => {
+            {store.coins.map(coin => {
               return (
                 <Link key={coin.id} to={`/${coin.id}`}>
                   <ListItem coin={coin} />
